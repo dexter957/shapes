b77e8445cf17d56b6acece45d8ca3452951cc2ba
dexter957 <ileanna957@gmail.com>
2018-02-25 17:55:35 +0200

    utilities header

---
+++ utilities/utilities.h
@@ -0,0 +1,10 @@
+
+#define TRUE 1
+#define FALSE 0
+
+
+int circle(double cenTet, double cenTat, double radius, double x, double y);
+int ring(double cenTet, double cenTat, double bigRadius, double smallRadius, double x, double y);
+int semicircle(double cenTet, double cenTat, double radius, double x, double y, char* orien);
+int ellipse(double h, double k, double a, double b, double x, double y);
+int square(double cenTet, double cenTat, double radius, double x, double y);
