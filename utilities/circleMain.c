7fad87e87f2cdf0a858f0a89497ee152c5515258
dexter957 <ileanna957@gmail.com>
2018-02-25 17:51:31 +0200

    main for circle utility

---
+++ utilities/circleMain.c
@@ -0,0 +1,21 @@
+
+#include "utilsExtra.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*If pointsToReadCount is not specified, then one is supposed to read the entire file*/
+
+int main(int argc,char* argv[])
+{
+	/*Now check if you have enough command line arguments*/
+	if((argc-1)<8)
+	{
+		printf("Error; not enough arguments\n");
+		exit(1);
+	}
+	else
+	{
+		utilityUse(CIRCLE_CASE, argc, argv);
+	}
+}
